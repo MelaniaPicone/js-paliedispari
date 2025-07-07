5d482fb74c4c48c4cc1a78eafa33c7c56c4f8d1d
MelaniaPicone <melania_picone@yahoo.it>
2025-07-07 21:41:29 +0200

    [fix] sum

--- paliedispari/js/script.js
+++ paliedispari/js/script.js
@@ -1,7 +1,5 @@
 //scelta pari o dispari utente
 const choose = prompt('Scegli fra pari e dispari');
-const choiceEven = ('pari');
-const choiceOdd = ('dispari');
 console.log(choose);
 
 // inserire numero da parte dell'utente
@@ -9,40 +7,32 @@ const userNum = parseInt(prompt('Inserisci un numero da 1 a 5')) ;
 console.log(userNum);
 
 // funzione per generare numero da uno a cinque
-function generateNum(min,max) {
+function generateNum(min, max) {
 return Math.floor(Math.random() * (max-min +1));
 }
 
-console.log(generateNum(1,5));
+// variabile numero random del pc
+const pcNum = generateNum(1,5);
+console.log(pcNum);
 
 // somma dei due numeri: utente + random
-let sum = userNum + generateNum(1,5);
+const sum = userNum + pcNum;
 console.log (sum);
 
 // funzione con if - else interno per dichiarare se la somma è pari o dispari
 function evenorOdd() {
   if (sum % 2 == 0) {
-    console.log('La somma ha un risultato pari');
+    console.log('Pari');
   }
+
   else {
-    console.log('La somma ha un risultato dispari')
+    console.log('Dispari');
   }
+
   return;
 }
 
 // chiamata della funzione
 evenorOdd();
 
-// dichiarazione della vittoria
-if (sum % 2 == 0 && choiceEven) {
-  console.log('Hai vinto')
-}
-
-else if (sum % 2 != 0 && choiceOdd) {
-  console.log ('Hai vinto')
-}
-
-else {
-console.log('Hai perso')
-}
 
